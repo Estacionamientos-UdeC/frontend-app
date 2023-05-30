18c9e678ebae7dc008707e56f2ada8179d98723b
mfurniel <mfurnielc@gmail.com>
2023-05-29 21:01:58 -0400

    avance de la pantalla login, se intenta crear el splash screen pero solo se puede cambiar el color del background el logo de flutter no desaparece y otras cosas

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-
+import 'package:frontend_app/utils/colors.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   _LoginPageState createState() => _LoginPageState();
@@ -13,42 +13,61 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController _passwordController = TextEditingController();
 
   @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
+      backgroundColor: azulUdec,
+      body: Column(
+        children: [
+          Image.asset("assets/images/login_image.png"),
+          Container(height: 4,color: Colors.black,),
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 10.0, right: 40.0, left: 40.0,top: 10.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Title(
+                    color: Colors.white, 
+                    child: Text(
+                      "Ingresar",
+                      style: TextStyle(
+                        color: Colors.white))),
+                  TextField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  TextField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                    ),
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 16.0),
+                  ElevatedButton(
+                    child: Text('Aceptar'),
+                    onPressed: () {
+                      String email = _emailController.text;
+                      String password = _passwordController.text;
+                      // Aquí puedes agregar la lógica de autenticación y redireccionar al usuario si los datos son válidos.
+                      print('Email: $email\nPassword: $password');
+                    },
+                  ),
+                ],
               ),
             ),
-            SizedBox(height: 16.0),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                labelText: 'Password',
-              ),
-              obscureText: true,
-            ),
-            SizedBox(height: 16.0),
-            MaterialButton(
-              child: Text('Login'),
-              onPressed: () {
-                String email = _emailController.text;
-                String password = _passwordController.text;
-                // Aquí puedes agregar la lógica de autenticación y redireccionar al usuario si los datos son válidos.
-                print('Email: $email\nPassword: $password');
-              },
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -10,3 +10,8 @@ const amarilloSuave = Color.fromARGB(255, 255, 230, 153);
 const rojoIntenso = Color.fromARGB(255, 255, 0, 0);
 const rojoClaro = Color.fromARGB(255, 255, 102, 102);
 
+const azulUdec = Color.fromARGB(255, 2, 48, 89);
+const rojoUdec = Color.fromARGB(255, 217, 11, 49);
+const amarilloUdec = Color.fromARGB(255, 242, 163, 65);
+const naranjaUdec = Color.fromARGB(255, 242, 125, 22);
+const negroUdec = Color.fromARGB(255, 13, 13, 13);
